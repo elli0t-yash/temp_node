390bce51077898e071cb9f3ea5237916f38df326
elli0t-yash <dev.yash.git@gmail.com>
2021-04-24 20:28:23 +0530

    new6

--- 16-promises.js
+++ 16-promises.js
@@ -1,29 +1,39 @@
-const { readFile } = require('fs')
-const { result } = require('lodash')
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err,data) => {
-            if(err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
-
-// getText('./content/first.txt')
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
+const { readFile, writeFile } = require('fs').promises
+// const { result } = require('lodash')
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const start = async () => {
     try {
-        const first = await getText('./content/first.txt')
-        console.log(first)
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        await writeFile(
+            './content/result-mind-grenade.txt',
+            `This is awesome : ${first} ${second}`,
+            { flag: 'a' }
+        )
+        console.log(first, second)
     } catch (error) {
         console.log(error)
     }
 }
 
 start()
+
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err,data) => {
+//             if(err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
